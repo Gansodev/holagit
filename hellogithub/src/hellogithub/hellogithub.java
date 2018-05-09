cdd05d56d551d32c7f4b156fd4fe1d0c73896a07
Vespertino <Vespertino@C8PC01>
2018-05-09 17:30:17 +0200

    Version1

---
+++ hellogithub/src/hellogithub/hellogithub.java
@@ -0,0 +1,10 @@
+package hellogithub;
+
+public class hellogithub {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola github");
+	}
+
+}
